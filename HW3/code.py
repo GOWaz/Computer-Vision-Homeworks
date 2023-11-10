759e49d6f20b15280bb43a23a8c5bd6c6a762682
GOWaz <abdullahalzabadani@gmail.com>
2023-11-10 14:46:02 +0300

    the 4th

--- HW3/code.py
+++ HW3/code.py
@@ -22,11 +22,10 @@ def standard_hough_transform(image):
         cv.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
     cv.imshow('Standard Hough Transform', image)
-    cv.imwrite('output/1.jpg', image)
     cv.waitKey(2000)
 
 
-# Probabilistic Hough Transform:
+# Probabilistic Hough Transform:    
 def probabilistic_hough_transform(image):
     grey = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     edges = cv.Canny(grey, 125, 175)
@@ -37,16 +36,6 @@ def probabilistic_hough_transform(image):
         cv.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
     cv.imshow('Probabilistic Hough Transform', image)
-    cv.imwrite('output/2.jpg', image)
-    cv.waitKey(2000)
-
-
-def remove_text(image, maskk):
-    dst = cv.inpaint(image, maskk, 3, cv.INPAINT_TELEA)
-    cv.imshow("OriginalImage", image)
-    cv.imshow('mask', maskk)
-    cv.imshow('dst', dst)
-    cv.imwrite('output/noText.jpg', dst)
     cv.waitKey(2000)
 
 
@@ -61,10 +50,3 @@ image21 = cv.imread('assets/2.jpg')
 image22 = cv.imread('assets/2.jpg')
 standard_hough_transform(image21)
 probabilistic_hough_transform(image22)
-
-# The 4th Q
-bookCover = cv.imread('assets/bookCover.jpg')
-mask = cv.imread('assets/mask.jpg', 0)
-# bookCover = cv.resize(bookCover, (1276, 909))
-# mask = cv.resize(mask, (1276, 909))
-remove_text(bookCover, mask)
